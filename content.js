a6f5f4cc32d0ac0905bd1dd535ed051b8f70e8c0
Justyna Nosek <justyna@example.com>
2020-07-20 12:18:04 +0200

    descripe sections of math, header

---
+++ content.js
@@ -0,0 +1,160 @@
+var json = {
+    "math1": {
+        "title": "Matematyka Dział I",
+        "elements": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Belki numeryczne</li>
+  <li class="list-group-item">Szorstkie cyfry</li>
+  <li class="list-group-item">Belki numeryczne i cyfry</li>
+  <li class="list-group-item">Wrzeciona</li>
+  <li class="list-group-item">Cyfry i żetony</li>
+    <li class="list-group-item">Mata</li>
+</ul>`,
+        "goal": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Nauczenie liczenia do dziesięciu </li>
+  <li class="list-group-item">Nauczenie symboli liczb od 0 do 10</li>
+  <li class="list-group-item">Pisanie cyfr</li>
+  <li class="list-group-item">Wyjaśnienie znaczenia "zero"</li>
+  <li class="list-group-item">Przedstawie wizualne liczb parzystych i nieparzystych</li>
+  <li class="list-group-item">Trenowanie pamięci przez zapamiętywanie liczb</li>
+</ul>`,
+
+    },
+    "math2": {
+        "title": "Matematyka Dział II",
+
+        "elements": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Taca prezenatcji, a na niej złote perły:sześcian tysiąca, kwadrat setki, dziesiątka i jedna perła jedności </li>
+  <li class="list-group-item">Taca prezentacji, z na niej złote perły: jeden tysiąc, dziewięć setek, dziewięć dziesiątek, dziewięć jedności</li>
+  <li class="list-group-item">Bank ze złotym materiałem: 45 tysięcy, 45 setek, 45 dzisiątek, 45 jednostek </li>
+  <li class="list-group-item">Taca z dużymi kartami liczbowymi: 1000, 100, 10, 1</li>
+  <li class="list-group-item">Jeden zestaw kart liczbowych od 1 do 9000</li>
+  <li class="list-group-item">Dwa zestawy małych kart liczbowych od 1 do 3000</li>
+  <li class="list-group-item">Różne tace prezentacji</li>
+    <li class="list-group-item">Mata</li>
+  
+</ul>`,
+        "goal": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Zapoznanie dziecka z nazwami różnych kategorii i zaznajomienie go z relatywnymi różnicami wielkości kategorii,
+   np. różnicą między ilością pięciu jednostek i pięciu tysięcy</li>
+  <li class="list-group-item">Nauczenie, że dziesięć sztuk niższej kategorii równa się jednej sztuce z wyższej kategorii</li>
+  <li class="list-group-item">Rozumienie struktury i hierarchii układu dziesiętnego</li>
+  <li class="list-group-item">Zapoznanie dziecka z symbolami 1000 i 100</li>
+  <li class="list-group-item">Dopasowanie cyfr do odpowiednich ilości</li>
+  <li class="list-group-item">Rozumienie hierarchii układu dziesiętnego</li>
+  <li class="list-group-item">Nauczenie dodawania</li>
+  <li class="list-group-item">Nauczenie mnożenia</li>
+  <li class="list-group-item">Nauczenie odejmowania</li>
+  <li class="list-group-item">Nauczenie dzielenia</li>
+  
+</ul>`
+    },
+    "math3": {
+        "title": "Matematyka Dział III",
+
+        "elements": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Pudełko z dziesięcioma złotymi dziesiątkami i zestaw kolorowych pereł</li>
+  <li class="list-group-item">Tablica Seguina I</li>
+  <li class="list-group-item">45 złotych dziesiątek</li>
+  <li class="list-group-item">Tablica Seguina II</li>
+  <li class="list-group-item">Pudełko złotych pereł zawierające dziewięć złotych dziesiątek i dziewięć złotych jedności</li>
+  <li class="list-group-item">Mata</li>
+  
+  
+</ul>`,
+        "goal": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Zapoznanie dziecka z nazwami różnych kategorii i zaznajomienie go z relatywnymi różnicami wielkości kategorii,
+   np. różnicą między ilością pięciu jednostek i pięciu tysięcy</li>
+  <li class="list-group-item">Nauczenie, że dziesięć sztuk niższej kategorii równa się jednej sztuce z wyższej kategorii</li>
+  <li class="list-group-item">Rozumienie struktury i hierarchii układu dziesiętnego</li>
+  <li class="list-group-item">Zapoznanie dziecka z symbolami 1000 i 100</li>
+  <li class="list-group-item">Dopasowanie liczb do odpowiednich ilości</li>
+  <li class="list-group-item">Rozumienie hierarchii układu dziesiętnego</li>
+  <li class="list-group-item">Nauczenie dodawania</li>
+  <li class="list-group-item">Nauczenie mnożenia</li>
+  <li class="list-group-item">Nauczenie odejmowania</li>
+  <li class="list-group-item">Nauczenie dzielenia</li>
+</ul>`
+    },
+    "math4": {
+        "title": "Matematyka Dział IV",
+
+        "elements": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Gra w znaczki: małe kolorowe płytki lub "znaczki" z symbolami: zielone z 1, niebieskie z 10, czerwone z 100, zielone z 1000</li>
+  <li class="list-group-item">Pionki: 1 duży zielony, po 9 czerwonych, niebieskich i zielonych</li>
+  <li class="list-group-item">Kilka plastikowych kręgów czerwonych, zielonych i niebieskich </li>
+  <li class="list-group-item">Papier w kratkę, kredki, linijka</li>
+  <li class="list-group-item">1, 10, 100, 1000 - złote perły</li>
+   
+  
+</ul>`,
+        "goal": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z dodawania w formie ćwiczeń indywidualnych</li>
+  <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z odejmowania w formie ćwiczeń indywidualnych</li>
+  <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z mnożenia w formie ćwiczeń indywidualnych</li>
+  <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z dzielenia w formie ćwiczeń indywidualnych</li>
+  <li class="list-group-item">Ilustracja mechanizmów układu dziesiętnego</li>
+  <li class="list-group-item">Skupienie uwagi dziecka na mechaniźmie wymiany</li>
+  <li class="list-group-item">Przygotowanie dziecka abstrakcyji</li> 
+</ul>`
+    },
+    "math5": {
+        "title": "Matematyka Dział V",
+
+        "elements": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Złoty łańcuch 100 składający się z 10 dziesiątek</li>
+  <li class="list-group-item">Pudełko zawierające etykiety: od 1 do 9 zielone, dziesiątki niebieskie, jedną czerwoną 100. Złoty kwadrat 100. </li>
+  <li class="list-group-item">Złoty łańcuch 1000 składający się z 100 dziesiątek </li>
+  <li class="list-group-item">Dziesięć złotych kwadratów 100 i jedna złota kostka 1000</li>
+  <li class="list-group-item">Pojemnik z kolorowymi perłami</li>
+  <li class="list-group-item">Zestaw od 1 do 9 czarno-białych łańcuchów pereł w czarno-białym pudełku</li>
+  <li class="list-group-item">Mała karbowana karta ("most")</li>
+  <li class="list-group-item">Tablica oraz tebele dodawania</li>
+  <li class="list-group-item">Tablica oraz tebele odejmowania</li>
+  <li class="list-group-item">Tablica oraz tebele mnożenia</li>
+  <li class="list-group-item">Tablica oraz tebele dzielenia</li>
+  <li class="list-group-item">Mata</li>
+  <li class="list-group-item">Długi dywanik</li>
+   
+  
+</ul>`,
+        "goal": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Utrwalenie umiejętności liczenia</li>
+  <li class="list-group-item">Przygotowanie do podnoszenia do kwadratu</li>
+  <li class="list-group-item">Zapoznanie dziecka z wszystkimi możliwymi kombinacjami tworzącymi dziesiatkę</li>
+  <li class="list-group-item">Pokazanie dziecku wszystkich mozliwych kombinacji w dodawaniu od 2 do 18</li>
+  <li class="list-group-item">Zapamiętywanie kombinacji dodawanie</li>
+  <li class="list-group-item">Zapoznanie dziecka z kombinacjami odejmowania</li>
+  <li class="list-group-item">Zapamiętywanie kombinacji odejmowania</li> 
+  <li class="list-group-item">Wytumaczenie i zapamietywanie tabliczki mnożenia</li> 
+  <li class="list-group-item">Nauczenie kombinacji dzielenia</li> 
+</ul>`
+    },
+
+}
+
+$('document').ready(function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    console.log(id);
+    const jsonProp = Object.getOwnPropertyNames(json);
+    console.log(jsonProp)
+    const title = document.querySelector('.title')
+    const text = document.getElementById('text')
+    const description = document.getElementById('elements');
+    const ingrediens = document.getElementById('goal');
+
+    for (let i = 0; i < jsonProp.length; i++) {
+        console.log(jsonProp[i])
+        if (id === jsonProp[i]) {
+            //
+            // image.src = json[jsonProp[i]]['src'];
+
+            description.innerHTML = json[jsonProp[i]]['elements'];
+            title.innerHTML = json[jsonProp[i]]['title'];
+            ingrediens.innerHTML = json[jsonProp[i]]['goal'];
+
+            // }
+        }
+    }
+
+});
