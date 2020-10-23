a8661c972480f7a06c57c5b605b7fa9cbbf98b6e
Justyna Nosek <justyna@example.com>
2020-10-23 14:21:24 +0200

    landing page mental wandering

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 var json = {
-    "math1": {
-        "title": "Matematyka Dział I",
-        "elements": `<ul class="list-group list-group-flush">
+  "math1": {
+    "title": "Matematyka Dział I",
+    "elements": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Belki numeryczne</li>
   <li class="list-group-item">Szorstkie cyfry</li>
   <li class="list-group-item">Belki numeryczne i cyfry</li>
@@ -9,7 +9,7 @@ var json = {
   <li class="list-group-item">Cyfry i żetony</li>
     <li class="list-group-item">Mata</li>
 </ul>`,
-        "goal": `<ul class="list-group list-group-flush">
+    "goal": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Nauczenie liczenia do dziesięciu </li>
   <li class="list-group-item">Nauczenie symboli liczb od 0 do 10</li>
   <li class="list-group-item">Pisanie cyfr</li>
@@ -17,12 +17,15 @@ var json = {
   <li class="list-group-item">Przedstawie wizualne liczb parzystych i nieparzystych</li>
   <li class="list-group-item">Trenowanie pamięci przez zapamiętywanie liczb</li>
 </ul>`,
+"text": `Kurs przeznaczony jest dla rodziców oraz nauczycieli Montessori. Jeżeli jesteś rodzicem i nie\n' +
+  '        masz pomocy Montessori to zachęcam Cię do zrobienia ich samemu w domu.\n' +
+  '        Do wykonania ich możesz użyć kartonu, pędzla, nożeczek, farb, metra, papieru ściernego, guziczków, klej wikol\n' +
+  '        oraz kasza kukurydziana (do szorstkich cyfr).`
+  },
+  "math2": {
+    "title": "Matematyka Dział II",
 
-    },
-    "math2": {
-        "title": "Matematyka Dział II",
-
-        "elements": `<ul class="list-group list-group-flush">
+    "elements": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Taca prezenatcji, a na niej złote perły:sześcian tysiąca, kwadrat setki, dziesiątka i jedna perła jedności </li>
   <li class="list-group-item">Taca prezentacji, z na niej złote perły: jeden tysiąc, dziewięć setek, dziewięć dziesiątek, dziewięć jedności</li>
   <li class="list-group-item">Bank ze złotym materiałem: 45 tysięcy, 45 setek, 45 dzisiątek, 45 jednostek </li>
@@ -33,25 +36,25 @@ var json = {
     <li class="list-group-item">Mata</li>
   
 </ul>`,
-        "goal": `<ul class="list-group list-group-flush">
+    "goal": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Zapoznanie dziecka z nazwami różnych kategorii i zaznajomienie go z relatywnymi różnicami wielkości kategorii,
    np. różnicą między ilością pięciu jednostek i pięciu tysięcy</li>
   <li class="list-group-item">Nauczenie, że dziesięć sztuk niższej kategorii równa się jednej sztuce z wyższej kategorii</li>
   <li class="list-group-item">Rozumienie struktury i hierarchii układu dziesiętnego</li>
   <li class="list-group-item">Zapoznanie dziecka z symbolami 1000 i 100</li>
   <li class="list-group-item">Dopasowanie cyfr do odpowiednich ilości</li>
-  <li class="list-group-item">Rozumienie hierarchii układu dziesiętnego</li>
+  <li class="list-group-item">Nauczenie języka związanego z dużymi liczbami</li>
   <li class="list-group-item">Nauczenie dodawania</li>
   <li class="list-group-item">Nauczenie mnożenia</li>
   <li class="list-group-item">Nauczenie odejmowania</li>
   <li class="list-group-item">Nauczenie dzielenia</li>
-  
+ 
 </ul>`
-    },
-    "math3": {
-        "title": "Matematyka Dział III",
+  },
+  "math3": {
+    "title": "Matematyka Dział III",
 
-        "elements": `<ul class="list-group list-group-flush">
+    "elements": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Pudełko z dziesięcioma złotymi dziesiątkami i zestaw kolorowych pereł</li>
   <li class="list-group-item">Tablica Seguina I</li>
   <li class="list-group-item">45 złotych dziesiątek</li>
@@ -61,24 +64,18 @@ var json = {
   
   
 </ul>`,
-        "goal": `<ul class="list-group list-group-flush">
-  <li class="list-group-item">Zapoznanie dziecka z nazwami różnych kategorii i zaznajomienie go z relatywnymi różnicami wielkości kategorii,
-   np. różnicą między ilością pięciu jednostek i pięciu tysięcy</li>
-  <li class="list-group-item">Nauczenie, że dziesięć sztuk niższej kategorii równa się jednej sztuce z wyższej kategorii</li>
-  <li class="list-group-item">Rozumienie struktury i hierarchii układu dziesiętnego</li>
-  <li class="list-group-item">Zapoznanie dziecka z symbolami 1000 i 100</li>
-  <li class="list-group-item">Dopasowanie liczb do odpowiednich ilości</li>
-  <li class="list-group-item">Rozumienie hierarchii układu dziesiętnego</li>
-  <li class="list-group-item">Nauczenie dodawania</li>
-  <li class="list-group-item">Nauczenie mnożenia</li>
-  <li class="list-group-item">Nauczenie odejmowania</li>
-  <li class="list-group-item">Nauczenie dzielenia</li>
-</ul>`
-    },
-    "math4": {
-        "title": "Matematyka Dział IV",
+    "goal": `<ul class="list-group list-group-flush">
+  <li class="list-group-item">Nauczenie nazw od 11 do 19 i skojarzenie ich z odpowiednią ilością oraz z ich symbolami</li>
+  <li class="list-group-item">Nauczenie nazw "dwadzieścia", "trzydzieści" itd.</li>
+  <li class="list-group-item">Nauczenie nazw "dwadzieścia", "trzydzieści" itd. i skojarzenie nazwy z ilością i symbolem liczby</li>
+  <li class="list-group-item">Nauczenie terminoogii od 11 do 99 - ilości i symbole</li>
+  <li class="list-group-item">Przygotowanie do liczenia co którąś liczbę</li>
+ </ul>`
+  },
+  "math4": {
+    "title": "Matematyka Dział IV",
 
-        "elements": `<ul class="list-group list-group-flush">
+    "elements": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Gra w znaczki: małe kolorowe płytki lub "znaczki" z symbolami: zielone z 1, niebieskie z 10, czerwone z 100, zielone z 1000</li>
   <li class="list-group-item">Pionki: 1 duży zielony, po 9 czerwonych, niebieskich i zielonych</li>
   <li class="list-group-item">Kilka plastikowych kręgów czerwonych, zielonych i niebieskich </li>
@@ -87,20 +84,20 @@ var json = {
    
   
 </ul>`,
-        "goal": `<ul class="list-group list-group-flush">
+    "goal": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z dodawania w formie ćwiczeń indywidualnych</li>
   <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z odejmowania w formie ćwiczeń indywidualnych</li>
   <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z mnożenia w formie ćwiczeń indywidualnych</li>
   <li class="list-group-item">Możliwość przeprowadzenia przez dziecko czterech działań z dzielenia w formie ćwiczeń indywidualnych</li>
   <li class="list-group-item">Ilustracja mechanizmów układu dziesiętnego</li>
   <li class="list-group-item">Skupienie uwagi dziecka na mechaniźmie wymiany</li>
-  <li class="list-group-item">Przygotowanie dziecka abstrakcyji</li> 
+  <li class="list-group-item">Przygotowanie dziecka do abstrakcyji</li> 
 </ul>`
-    },
-    "math5": {
-        "title": "Matematyka Dział V",
+  },
+  "math5": {
+    "title": "Matematyka Dział V",
 
-        "elements": `<ul class="list-group list-group-flush">
+    "elements": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Złoty łańcuch 100 składający się z 10 dziesiątek</li>
   <li class="list-group-item">Pudełko zawierające etykiety: od 1 do 9 zielone, dziesiątki niebieskie, jedną czerwoną 100. Złoty kwadrat 100. </li>
   <li class="list-group-item">Złoty łańcuch 1000 składający się z 100 dziesiątek </li>
@@ -117,7 +114,7 @@ var json = {
    
   
 </ul>`,
-        "goal": `<ul class="list-group list-group-flush">
+    "goal": `<ul class="list-group list-group-flush">
   <li class="list-group-item">Utrwalenie umiejętności liczenia</li>
   <li class="list-group-item">Przygotowanie do podnoszenia do kwadratu</li>
   <li class="list-group-item">Zapoznanie dziecka z wszystkimi możliwymi kombinacjami tworzącymi dziesiatkę</li>
@@ -128,33 +125,34 @@ var json = {
   <li class="list-group-item">Wytumaczenie i zapamietywanie tabliczki mnożenia</li> 
   <li class="list-group-item">Nauczenie kombinacji dzielenia</li> 
 </ul>`
-    },
+  },
 
 }
 
 $('document').ready(function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    console.log(id);
-    const jsonProp = Object.getOwnPropertyNames(json);
-    console.log(jsonProp)
-    const title = document.querySelector('.title')
-    const text = document.getElementById('text')
-    const description = document.getElementById('elements');
-    const ingrediens = document.getElementById('goal');
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get('id');
+  console.log(id);
+  const jsonProp = Object.getOwnPropertyNames(json);
+  console.log(jsonProp)
+  const title = document.querySelector('.title')
+  const text = document.getElementById('text')
+  const description = document.getElementById('elements');
+  const ingrediens = document.getElementById('goal');
 
-    for (let i = 0; i < jsonProp.length; i++) {
-        console.log(jsonProp[i])
-        if (id === jsonProp[i]) {
-            //
-            // image.src = json[jsonProp[i]]['src'];
+  for (let i = 0; i < jsonProp.length; i++) {
+    console.log(jsonProp[i])
+    if (id === jsonProp[i]) {
+      //
+      // image.src = json[jsonProp[i]]['src'];
 
-            description.innerHTML = json[jsonProp[i]]['elements'];
-            title.innerHTML = json[jsonProp[i]]['title'];
-            ingrediens.innerHTML = json[jsonProp[i]]['goal'];
+      description.innerHTML = json[jsonProp[i]]['elements'];
+      title.innerHTML = json[jsonProp[i]]['title'];
+      ingrediens.innerHTML = json[jsonProp[i]]['goal'];
+      text.innerHTML = json[jsonProp[i]]['text']
 
-            // }
-        }
+      // }
     }
+  }
 
 });
